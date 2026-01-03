d2f7dcf33bbdd7bd46b02ef680b5df040cd0bec6
waldls <p353700@gmail.com>
2026-01-03 13:43:33 +0000

    [CHORE] build.sh 수정

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd ../
 mkdir output
-cp -R ./Homepage_client_ver2.0/* ./output
-cp -R ./output ./Homepage_client_ver2.0/
+cp -R ./Homepage_client_ver3.0/* ./output
+cp -R ./output ./Homepage_client_ver3.0/
