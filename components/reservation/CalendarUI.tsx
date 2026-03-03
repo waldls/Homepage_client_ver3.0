3599da04180931a29a67e5139569818626aea561
waldls <p353700@gmail.com>
2026-03-03 04:33:15 +0000

    [FIX] 동방 예약 시간 변경

--- components/reservation/CalendarUI.tsx
+++ components/reservation/CalendarUI.tsx
@@ -90,8 +90,8 @@ const CalendarUI = ({ onChange }: CalendarProps) => {
     // // 오늘 기준 2주 이후 날짜는 비활성화
     // if (compareDate > twoWeeksFromToday) return false;
 
-    // 월, 목, 토, 일요일만 선택 가능 (2025년 2학기)
-    return day === 1 || day === 4 || day === 6 || day === 0;
+    // 화, 수, 금, 일요일만 선택 가능 (2026년 1학기)
+    return day === 2 || day === 3 || day === 5 || day === 0;
   };
 
   return (
